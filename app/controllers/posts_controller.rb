f5fd7363092bd57f9415ec6244f67e8c3327a743
omri-simplee <omri@simplee.com>
2017-02-16 11:10:15 -0800

    Added the option to create new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,8 +8,24 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def edit
+  end
+
+def create
+    params2 = params[:post]
+    @post = Post.new(:title=>params2[:title], :body=>params2[:body])
+    # @post = Post.new(params2[:title], params2[:body])
+    # @post = Post.new(params2.(:title, :body))
+    # Rails.logger.debug(params.to_s)
+    if @post.save
+      flash[:notice] = "Post was saved."
+      redirect_to @post
+    else
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :new
+    end
   end
 end
