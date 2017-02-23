fb4b72f48b6835ce6a5bacd6abdba26c4a072c36
omri-simplee <omri@simplee.com>
2017-02-23 13:47:14 -0800

    Added scopes to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
   default_scope { order('created_at DESC') }
+  scope :ordered_by_title, -> {order('title DESC')}
+  scope :order_by_reverse_created_at, -> {order('created_at ASC')}
 end
