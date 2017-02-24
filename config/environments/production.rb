949a82a2e205209e2a5fe6f18d32490bb2c6d6ad
omri-simplee <omri@simplee.com>
2017-02-24 11:33:31 -0800

    Updated production env email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Dojit::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
   config.eager_load = true
-  config.action_mailer.default_url_options = { host: 'Dojit.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'omri-dojit.herokuapp.com' }
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
