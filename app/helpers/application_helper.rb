e371ea18060c765db35439593aed1fc318bedaee
omri-simplee <omri@simplee.com>
2017-02-28 14:36:51 -0800

    Finished validation posts, added helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,9 @@
 module ApplicationHelper
-	MyName = 'Omri Attoun'
-	def my_name
-		MyName
-	end
+  def form_group_tag(errors, &block)
+    if errors.any?
+      content_tag :div, capture(&block), class: 'form-group has-error'
+    else
+      content_tag :div, capture(&block), class: 'form-group'
+    end
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,12 @@ class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :topic
   default_scope { order('created_at DESC') }
+
+  validates :title, length: {minimum: 5}, presence: true
+  validates :body, length: {maximum: 20}, presence: true
+  validates :user, presence: true
+  validates :topic, presence: true
+
   scope :ordered_by_title, -> {order('title DESC')}
   scope :order_by_reverse_created_at, -> {order('created_at ASC')}
 end
